f0bb75133813e4ace726427e77f9c9bd8c42bc82
Cobb-Yoo <yhj010597@naver.com>
2022-03-16 21:59:14 +0900

    interior init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,76 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <template v-slot:activator>
+          <v-list-item-title>등록</v-list-item-title>
+        </template>
+
+        <v-list-item
+          v-for="register in registers"
+          :key="register.title"
+          link
+          @click.stop="drawer = !drawer"
+          :to="register.to"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ register.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ register.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    drawer: false,
+    registers: [
+      {
+        title: "홈",
+        icon: "mdi-view-dashboard",
+        to: "/",
+      },
+      {
+        title: "어바웃",
+        icon: "mdi-view-dashboard",
+        to: "/about",
+      },
+    ],
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,335 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-title> 1번 타이틀 </v-card-title>
+        </v-card>
+      </v-col>
+
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-row>
+
+    <v-row justify="center" align="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-title> 2번 타이틀 </v-card-title>
+        </v-card>
+      </v-col>
+
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
